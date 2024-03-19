339229c57a1b9234f02f1c018bb7ff2e838511b8
Sujatagoyat <sujatagoyat8@gmail.com>
2024-03-19 11:49:28 +0530

    footer section done

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,19 +11,21 @@ menuIcon.addEventListener("click", function () {
 // =================== slider ===============================================
 $('.responsive').slick({
     dots: true,
-    infinite: false,
+    infinite: true,
+    autoplay:true,
+    autoplaySpeed:1000,
+    ltr:true,
     speed: 300,
-   
     slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToScroll: 1,
     responsive: [
         {
             breakpoint: 1024,
             settings: {
                 slidesToShow: 3,
                 slidesToScroll: 1,
-                // infinite: true,
-                dots:true,
+                infinite: true,
+                dots:false,
                
             }
         },
@@ -46,3 +48,56 @@ $('.responsive').slick({
         // instead of a settings object
     ]
 });
+$('.responsive-2').slick({
+    dots: true,
+    infinite: true,
+    autoplay:true,
+    autoplaySpeed:1000,
+    rtl:true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots:false,
+               
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
+// ====================== back to top =============================
+const topBtn = document.querySelector(".topBtn");
+const show = document.querySelector(".show");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 60) {
+        topBtn.classList.add("show");
+    } else {
+        topBtn.classList.remove("show");
+    }
+})
+topBtn.addEventListener("click", function () {
+    document.documentElement.scrollTop = 0;
+})
